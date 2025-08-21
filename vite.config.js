41bb328eded31077873d36c19010b5b6c40b745b
Andrey Zhukov <azhukov@idscan.net>
2025-08-21 11:36:31 +0500

    fix: replace idv2 library

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,19 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import copy from 'rollup-plugin-copy';
 import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
-  server: { https: true, port: 5012 },
+  server: {https: true, port: 5012},
   build: {
     rollupOptions: {
-      external: [        /^@idscan:.*/,
+      external: [/^@idscan:.*/,
       ],
     }
   },
-    plugins: [
+  plugins: [
     copy({
       targets: [
-        { src: 'node_modules/@idscan/idvc2/dist/networks/*', dest: 'public/networks' },
+        {src: 'node_modules/@idscan/idvc2/dist/networks/*', dest: 'public/networks'},
       ],
       hook: 'writeBundle',
     }),
